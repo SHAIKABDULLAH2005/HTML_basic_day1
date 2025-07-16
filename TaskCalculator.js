de684fe4bd81ecdecb04697b1513acad7fafa8c3
SHAIKABDULLAH <144833376+SHAIKABDULLAH2005@users.noreply.github.com>
2025-07-16 09:14:18 +0530

    Add files via upload

---
+++ TaskCalculator.js
@@ -0,0 +1,10 @@
+var a=20
+var b=10
+var sum=a+b
+console.log("the sum two number is: "+sum)
+var difrence=a-b
+console.log("the diffrence of two number is: "+difrence)
+var multi=a*b
+console.log("the multiple of two number is: "+multi)
+var div=a/b
+console.log("The divison of two number is: "+div)
