652c5fe94a0284389ed2189559e1a5d758bc03f3
SHAIKABDULLAH <144833376+SHAIKABDULLAH2005@users.noreply.github.com>
2025-07-17 19:18:34 +0530

    Add files via upload

---
+++ TaskGreatestThree.js
@@ -0,0 +1,13 @@
+let a="30 is Greater than"
+let b="20 is Greater than"
+let c="10 is Greater than"
+if(a>b){
+    console.log (b>a)
+if (b>c) {
+    console.log(b>c)
+    
+}else {
+    console.log(a>c)
+    
+}
+}
