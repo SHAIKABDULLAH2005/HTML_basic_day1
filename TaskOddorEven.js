fe6d308230b4758b4f2966690af473a1f86cd5f5
SHAIKABDULLAH <144833376+SHAIKABDULLAH2005@users.noreply.github.com>
2025-07-17 19:39:14 +0530

    Add files via upload

---
+++ TaskOddorEven.js
@@ -0,0 +1,9 @@
+for(let i=1;i<=10;i++){
+    if (i%5 ===0){
+        console.log('${i} is divisible by 5');
+    } else if(i%2===0){
+        console.log('${i} is even');
+    } else{
+        console.log('${i} is odd');
+    }
+}
